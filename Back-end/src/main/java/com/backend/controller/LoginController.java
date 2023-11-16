e53c52d3c91989713977ca984f145e10d7b7151b
DawsonNat <115479065+DawsonNat@users.noreply.github.com>
2023-11-16 08:39:20 +0800

    jwt令牌下发
    jwt令牌下发测试

--- Back-end/src/main/java/com/backend/controller/LoginController.java
+++ Back-end/src/main/java/com/backend/controller/LoginController.java
@@ -2,6 +2,7 @@ package com.backend.controller;
 
 import com.backend.pojo.User;
 import com.backend.service.UserService;
+import com.backend.utils.JwtUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,6 +10,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import com.backend.pojo.Result;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Slf4j // 该注解用于记录日志
 @RestController
@@ -20,6 +24,20 @@ public class LoginController {
     public Result login(@RequestBody User user) { // @RequestBody注解就是将json格式的数据封装到实体类当中
         log.info("用户登录: {}", user); // 使用了日志记录工具，将登录操作的相关信息打印到日志中。
         User u = userService.login(user);
-        return u!=null?Result.success():Result.error("用户名或密码错误");
+
+        // 登录成功则生成令牌并下发令牌
+        if(u!=null){
+            Map<String, Object> claims = new HashMap<>();
+            claims.put("id", u.getId());
+            claims.put("username", u.getUsername());
+
+            String jwt = JwtUtils.generateJwt(claims); // 这里生成的jwt令牌包含了用户信息（用户名和id）
+
+            return Result.success(jwt); // 下发jwt令牌给前端
+        }
+
+        // 登录失败则返回错误信息
+
+        return Result.error("用户名或密码错误");
     }
 }
--- Back-end/src/test/java/com/backend/BackEndApplicationTests.java
+++ Back-end/src/test/java/com/backend/BackEndApplicationTests.java
@@ -1,5 +1,6 @@
 package com.backend;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.junit.jupiter.api.Test;
@@ -31,5 +32,17 @@ class BackEndApplicationTests {
                 .setExpiration(new Date(System.currentTimeMillis() + 3600*1000)) // 设置令牌有效时间为1个小时
                 .compact(); // 调用compact方法来返回一个字符串，该字符串就是令牌
         System.out.println(jwt);
+    }
+
+    /*
+    测试解析jwt令牌
+     */
+    @Test
+    public void testParseJwt() {
+        Claims claims = Jwts.parser()
+                .setSigningKey("ITSEP") // 传递解析密钥
+                .parseClaimsJws("eyJhbGciOiJIUzI1NiJ9.eyJuYW1lIjoidG9tIiwiaWQiOjEsImV4cCI6MTcwMDAzNDYwMH0.N1_WijP7TApK4U5To4FmnVG-3aMmdvAS_mt5_RJUdQw") // 传递jwt令牌(注意传递的令牌应该使用引号括起来)
+                .getBody(); // 获取解析后的数据主体
+        System.out.println(claims);
     }
 }
